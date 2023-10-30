4fce4d90e2a8abdbf7a09abe129d5a3292b8f83c
atsushi-kitazawa <47269784+atsushi-kitazawa@users.noreply.github.com>
2023-10-31 00:20:57 +0900

    update rag

--- rag.py
+++ rag.py
@@ -1,14 +1,37 @@
 from langchain.document_loaders import GitLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 
 def file_filter(file_path):
     return file_path.endswith('.mdx')
-    
+
+# document loader
 loader = GitLoader(
     clone_url='https://github.com/langchain-ai/langchain',
     repo_path='./langchain',
     branch='master',
     file_filter=file_filter,
 )
-
 raw_docs = loader.load()
-print(len(raw_docs))
+#print(len(raw_docs))
+
+# document transformers
+text_splitter = CharacterTextSplitter(chunk_size=10000, chunk_overlap=0)
+docs = text_splitter.split_documents(raw_docs)
+# print(len(docs))
+
+# text embedding & vector store
+embeddings = OpenAIEmbeddings()
+query = "AWSのS3からデータを読み込むためのDocumentLoaderはありますか？"
+vector = embeddings.embed_query(query)
+# print(len(vector))
+# print(vector)
+db = Chroma.from_documents(docs, embeddings)
+
+# Retrievers
+retriever = db.as_retriever()
+context_docs = retriever.get_relevant_documents(query)
+print(f"len = {len(context_docs)}")
+print(f"metadata = {context_docs[0].metadata}")
+print(context_docs[0].page_content)
